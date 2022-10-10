92e81764a4722efcccb0de589b22644622643d5d
Radityo Ardi <radityo.ardi@live.com>
2022-10-10 18:24:10 +0800

    no notes

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,15 @@
 import express, { Express, Request, Response } from 'express';
+import bodyParser from 'body-parser';
+import path from 'path';
+
+
+const buildDir = path.join(process.cwd() + "/build");
 const api: Express = express();
 const port = process.env.PORT || 80;
+
+api.use(bodyParser.json());
+api.use(bodyParser.urlencoded({ extended: true }));
+api.use(express.static(buildDir));
 
 /*
 */
